e0bc9b39197b91480426b30da9b6c2cb134f1f2f
Selman K <selmank@yahoo-inc.com>
2014-11-14 01:29:29 -0800

    updates

--- src/com/example/yahoosuperstar/MovieList.java
+++ src/com/example/yahoosuperstar/MovieList.java
@@ -1,11 +1,14 @@
 package com.example.yahoosuperstar;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -13,92 +16,34 @@ import android.widget.AdapterView.OnItemClickListener;
 
 import com.example.yahoosuperstar.util.SystemUiHider;
 
-
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- * 
- * @see SystemUiHider
- */
-public class MovieList extends Activity {
-	/**
-	 * Whether or not the system UI should be auto-hidden after
-	 * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-	 */
-	private static final boolean AUTO_HIDE = true;
-
-	/**
-	 * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-	 * user interaction before hiding the system UI.
-	 */
-	private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-	/**
-	 * If set, will toggle the system UI visibility upon interaction. Otherwise,
-	 * will show the system UI visibility upon interaction.
-	 */
-	private static final boolean TOGGLE_ON_CLICK = true;
-
-	/**
-	 * The flags to pass to {@link SystemUiHider#getInstance}.
-	 */
-	private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
-
-	/**
-	 * The instance of the {@link SystemUiHider} for this activity.
-	 */
-	private SystemUiHider mSystemUiHider;
+public class MovieList extends ListActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		//Remove title bar
+	    this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+	    //Remove notification bar
+	    this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 		setContentView(R.layout.activity_movie_list);
 
 		final View controlsView = findViewById(R.id.fullscreen_content_controls);
 		final View contentView = findViewById(R.id.fullscreen_content);
 
-		
-		 String[] Movies_list = new String[] { "SuperMan Returns","Batman Begins","The Hobbit","Maleficient"};
-		 
-		 ArrayAdapter<String> MovieListArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Movies_list);
-		 ListView MovieListItems =(ListView)findViewById(R.id.listView1);
-		 MovieListItems.setAdapter(MovieListArrayAdapter);
-		 
-		 MovieListItems.setOnItemClickListener(new OnItemClickListener() {
-
-				@Override
-				public void onItemClick(AdapterView<?> arg0, View v,int position, long id) {
-					
-					//configName = path+"/"+configList[position];
-					//System.out.println(" hey heyy Selecting config name at :" + configName);
-					
-					
-					//Intent selectionIntent = new Intent(v.getContext(),Trees.class);
-					Intent rolesIntent = new Intent(v.getContext(),Roles.class);
-					//selectionIntent.setData(Uri.parse(configName));
-					startActivity(rolesIntent);
-					
-					System.out.println("this is my selection now what??");
-					
-					
-					
-					
-				}
-			});
-			
-		 
-		 
-		 
-
+		 String[] Movies_list = new String[] { "Superman Returns (Audition)","Joe's Halloween Party","Y! New Year Party","Food Party @Jenny's"};
+         setListAdapter(new ArrayAdapter < String > (this,
+                 android.R.layout.simple_list_item_1, Movies_list));
+         getListView().setTextFilterEnabled(true);
 	}
 
-	Handler mHideHandler = new Handler();
-	Runnable mHideRunnable = new Runnable() {
-		@Override
-		public void run() {
-			mSystemUiHider.hide();
-		}
-	};
-
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        // TODO Auto-generated method stub
+        super.onListItemClick(l, v, position, id);
+		Intent rolesIntent = new Intent(v.getContext(), RoleDetailActivity.class);
+		startActivity(rolesIntent);
+    }
 }
--- src/com/example/yahoosuperstar/RoleDetailActivity.java
+++ src/com/example/yahoosuperstar/RoleDetailActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
@@ -15,10 +17,17 @@ public class RoleDetailActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		//Remove title bar
+	    this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+	    //Remove notification bar
+	    this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 		setContentView(R.layout.activity_role_detail);
 		
 		
-		button =(Button) findViewById(R.id.button1);
+		button = (Button) findViewById(R.id.button1);
 		button.setOnClickListener( new OnClickListener() {
 			
 			public void onClick(View v) {
